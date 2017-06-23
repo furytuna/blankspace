bb23b941f144ddd90bec0f04d738a435b7fc4418
Lattapon Yodsuwan <i@clozed2u.com>
2017-06-23 23:08:01 +0700

    เพิ่มสมุดลงทะเบียนกะลาสี - Add firebase facebook login

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Prologue from './Prologue'
 import Register from './Register'
+import firebase from 'firebase'
+import reactfire from 'reactfire'
 import {
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom'
+
+var config = {
+  apiKey: "AIzaSyCTk3XG7Zm_AsSlCxVbiS43kF3DAxMXmoY",
+  authDomain: "blankspace-3ffc8.firebaseapp.com",
+  databaseURL: "https://blankspace-3ffc8.firebaseio.com",
+  projectId: "blankspace-3ffc8",
+  storageBucket: "blankspace-3ffc8.appspot.com",
+  messagingSenderId: "395836399507"
+};
+firebase.initializeApp(config);
 
 const App = () => (
   <Router>
@@ -17,4 +29,4 @@ const App = () => (
   </Router>
 )
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App firstName="Lattapon" />, document.getElementById('root'))
