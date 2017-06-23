edce1cb203b811905410951cec39ccb7cfccd44b
Lattapon Yodsuwan <i@clozed2u.com>
2017-06-23 20:43:24 +0700

    ขึ้นโครงกระดูกงู ทำทางขึ้นเรือ - We just learnt to use react-router-dom - Add basic prologue and register components - God bless us!

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Prologue from './Prologue'
+import Register from './Register'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Prologue}/>
+      <Route path="/register" component={Register}/>
+    </div>
+  </Router>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
