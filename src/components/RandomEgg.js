38f0dfc2417847bd9836c66a50f3c0bbea0912a6
Hiruma <yapadubpadoo@gmail.com>
2017-06-24 18:16:48 +0700

    แก้ไขรายชื่อคนในปูมเรือ - change name

--- src/components/RandomEgg.js
+++ src/components/RandomEgg.js
@@ -26,12 +26,12 @@ export default class RandomEgg extends Component {
         <div className="random-egg-box">
           <div className="random-egg-bg box" />
           <div className="random-egg-content">
-              <h1 className="title"><b>เลือก Monster คู่กาย</b></h1>
-              Monster ตัวนี้จะคอยติดตามคุณไปทุกที่
+              <h1 className="title"><b>เลือก Tunagotchi คู่กาย</b></h1>
+              Tunagotchi ตัวนี้จะคอยติดตามคุณไปทุกที่
               มันจะร่วมต่อสู้และผจญภัยไปคุณ
           </div>
         </div>
-        <Link to='/sceneIncubate' className="button is-primary">สุ่ม Monster</Link>
+        <Link to='/sceneIncubate' className="button is-primary">สุ่ม Tunagotchi</Link>
         <audio autoPlay={true}>
             <source src="../assets/audio/intro.mp3" type="audio/mpeg"/>
         </audio>
