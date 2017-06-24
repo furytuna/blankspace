758d65799f1c1c3f102ed4686f074e479b2ff271
Lattapon Yodsuwan <i@clozed2u.com>
2017-06-24 23:02:56 +0700

    ทำลายร่องรอยการวางแผน - Remove console.log

--- src/reducers/music_player.js
+++ src/reducers/music_player.js
@@ -6,7 +6,6 @@ const initialState = "PLAY"
 const musicReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_MUSIC_STATUS: {
-      console.log('reducer', action.payload)
       let newState = action.payload.status
       localStorage.setItem('music_player', newState)
       firebase.database().ref('users/' + action.payload.uid + '/music_player').set(newState)
