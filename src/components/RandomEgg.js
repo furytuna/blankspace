8b5f0f890313ec042f51378db44547b156c89b8f
Patchara Suwanwanakul <salamtam.p@gmail.com>
2017-06-24 18:07:29 +0700

    ทาสีห้องเครื่อง - Edit CSS on scene random eggs

--- src/components/RandomEgg.js
+++ src/components/RandomEgg.js
@@ -4,10 +4,15 @@ import { Link } from 'react-router-dom'
 export default class RandomEgg extends Component {
   render() {
     return (
-      <div className="random-egg-wrapper">
-        <h1 className="title">เลือก Monster คู่กาย</h1>
-        <p>Monster ตัวนี้จะคอยติดตามคุณไปทุกที่</p>
-        <p>มันจะร่วมต่อสู้และผจญภัยไปคุณ</p>
+      <div className="random-egg-wrapper container has-text-centered column">
+        <div className="random-egg-box">
+          <div className="random-egg-bg box" />
+          <div className="random-egg-content">
+              <h1 className="title">เลือก Monster คู่กาย</h1>
+              Monster ตัวนี้จะคอยติดตามคุณไปทุกที่
+              มันจะร่วมต่อสู้และผจญภัยไปคุณ
+          </div>
+        </div>
         <Link to='/sceneIncubate' className="button is-primary">สุ่ม Monster</Link>
         <audio autoPlay={true}>
             <source src="../assets/audio/intro.mp3" type="audio/mpeg"/>
