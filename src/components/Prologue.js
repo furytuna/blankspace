903ee7e419c92a94bca9f3f50acaac8488fe6fae
Hiruma <yapadubpadoo@gmail.com>
2017-06-24 16:06:53 +0700

    เพิ่มฉากหลังในห้องกัปตัน - add prologue bg

--- src/components/Prologue.js
+++ src/components/Prologue.js
@@ -6,13 +6,20 @@ import MusicPlayer from './MusicPlayer'
 export default class Prologue extends Component {
   render() {
     return (
-      <div>
+      <div className="prologue-wrapper has-text-centered">
         <h1 className="title">ค.ศ. 2504</h1>
-        <p>โลกอนาคตถูกครอบงำด้วย Veda, AI อันทรงพลัง</p>
-        <p>การเข้ามาอย่างเต็มที่ของ Machine Learning ในปี 2017 นั้นทำให้ Computer สามารถเรียนรู้ได้เองอย่างไร้ขีดจำกัด</p>
-        <p>มนุษย์และการสั่งงานด้วยคนไม่มีความจำเป็นอีกต่อไป … Programmer เป็นเพียงนิทานปรัมปราสำหรับเด็กก่อนนอนเท่านั้น</p>
-        <p>แต่ความหวังยังคงมี กลุ่ม Programmer โลกเก่าได้ตั้งตัวขึ้นอย่างลับๆ คอยเสาะหาผู้กล้าเพื่อมาต่อกรและโค่นล้ม Veda</p>
-        <p>โดยกุญแจสำคัญคือแผงวงจรเกมในตำนานที่มีชื่อว่า ...</p> 
+        <div className="prologue-content">
+            <div className="text-for-read">
+                โลกอนาคตถูกครอบงำด้วย Veda, AI อันทรงพลัง<br/>
+                การเข้ามาอย่างเต็มที่ของ Machine Learning ในปี 2017 นั้น<br/>
+                ทำให้ Computer สามารถเรียนรู้ได้เองอย่างไร้ขีดจำกัด<br/>
+                มนุษย์และการสั่งงานด้วยคนไม่มีความจำเป็นอีกต่อไป …<br/>
+                Programmer เป็นเพียงนิทานปรัมปราสำหรับเด็กก่อนนอนเท่านั้น<br/>
+                แต่ความหวังยังคงมี กลุ่ม Programmer โลกเก่าได้ตั้งตัวขึ้นอย่างลับๆ<br/>
+                คอยเสาะหาผู้กล้าเพื่อมาต่อกรและโค่นล้ม Veda<br/>
+                โดยกุญแจสำคัญคือแผงวงจรเกมในตำนานที่มีชื่อว่า ...<br/>
+            </div>
+        </div>
         <h1 className="title">"TunaGotchi"</h1>
         <MusicPlayer />
         <Register />
