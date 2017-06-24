ee1b6a1403ef895cb02ae4f303001a50a2424db2
Lattapon Yodsuwan <i@clozed2u.com>
2017-06-25 02:19:50 +0700

    ต่อเติมเรือ - Change react-cloud-vision-api dependency

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -2,8 +2,12 @@ import { connect } from 'react-redux'
 import Register from '../components/Register'
 import { setProfile } from '../actions/profile'
 
+const mapStateToProps = (state) => ({
+  profile: state.profile
+})
+
 const mapDispatchToProps = (dispatch) => ({
   setProfile: (payload) => dispatch(setProfile(payload))
 })
 
-export default connect(null, mapDispatchToProps)(Register)
+export default connect(mapStateToProps, mapDispatchToProps)(Register)
