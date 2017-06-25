e3fbf9f1716ffe7fb45e3cbc21d28c0f55bb04ce
Patchara Suwanwanakul <salamtam.p@gmail.com>
2017-06-25 15:38:58 +0700

    ทาสีกล่องขนสัตว์ประหลาด - Edit CSS on monster

--- src/components/Monster.js
+++ src/components/Monster.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react'
 import MusicPlayer from './MusicPlayer'
-import SceneSwitcher from '../containers/SceneSwitcher'
+// import SceneSwitcher from '../containers/SceneSwitcher'
 
 class Monster extends Component {
   render() {
     let monster = this.props.monster.mature
     return (
       <div className="monster-wrapper container has-text-centered column">
-        <SceneSwitcher accessScene="sceneMonster" />
+        { /* <SceneSwitcher accessScene="sceneMonster" /> */ }
         <div className="monster-box">
-          <div className="monster-bg" />
-          <div className="monster-content">
-            <div className="monster-mature">
-              <img src={monster.src} alt={monster.name}/>
+          <div className="monster-bg">
+            <div className="monster-content">
+              <div className="monster-mature">
+                <img src={monster.src} alt={monster.name}/>
+              </div>
+              <div className="monster-detail box">
+                <h1 className="title is-4"><b>{monster.name}</b></h1>
+                <p>{monster.history}</p>
+              </div>
             </div>
-            <div className="monster-detail box">
-              <h1 className="title is-4"><b>{monster.name}</b></h1>
-              <p>{monster.history}</p>
-            </div>
+            <div className="monster-footer" />
           </div>
         </div>
         <MusicPlayer name="Monster"/>
